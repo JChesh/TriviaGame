b0af2331698f2d3cfe824ecbdc1b723316171e43
JChesh <jonathans.cheshire@gmail.com>
2019-07-11 20:13:02 -0400

    Basic outline + made variables and questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,63 @@
+//Hi
+
+
+//Created a variable trivia to hold the content
+var game = {
+
+    //Properties
+
+    //Right and Wrong answers
+    right: 0,
+    wrong: 0,
+    //Unawswerd and timer
+    incomplete: 0,
+    current: 0,
+    timer: 20,
+    timerProp: " ",
+    timerVal: false,
+
+    //Questions, true/false, answers
+    questions: {
+        a: "The Ford Mustang has a horse as its logo",
+        b: "The Audi r8 has an 4 cylinder engine",
+        c: "The Tesla is fully electric",
+        d: "BMW doesn't make an electric car",
+        e: "FIAT is an Italian car manufacturer",
+        f: "Ford F-150 is the #1 selling truck",
+    },
+    //Created True/False arrays
+    trueFalse: {
+        a: ["True", "False"],
+        b: ["True", "False"],
+        c: ["True", "False"],
+        d: ["True", "False"],
+        e: ["True", "False"],
+        f: ["True", "False"],
+    },
+    //Correct answers
+    answers: {
+        a: "True",
+        b: "False",
+        c: "True",
+        d: "False",
+        e: "True",
+        f: "True",
+    },
+
+//Start Game
+
+//Restart Game
+
+//Show Timer
+
+//Display Questions and choices
+
+//Set Timer every question
+
+//Count unanswered
+
+//Show Game Stats
+
+//Remove previous options and results
+
+}
